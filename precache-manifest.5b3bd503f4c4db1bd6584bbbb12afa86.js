f0339a8f20bd19ba53bac4407fafa6cfff0d024b
dhruv <dhruvr@exzeo.com>
2020-01-16 22:58:35 +0530

    Updates

---
+++ precache-manifest.5b3bd503f4c4db1bd6584bbbb12afa86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afe80e5e352307a73593b4afb79108d",
+    "url": "/dhruvpsaru/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b6dc2cad12b9343c3a16",
+    "url": "/dhruvpsaru/monsters-rolodex/static/css/main.1047cf13.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/dhruvpsaru/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dhruvpsaru/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6dc2cad12b9343c3a16",
+    "url": "/dhruvpsaru/monsters-rolodex/static/js/main.de81d075.chunk.js"
+  },
+  {
+    "revision": "22d472ee74ae214d35d7",
+    "url": "/dhruvpsaru/monsters-rolodex/static/js/runtime-main.aa1c9e19.js"
+  }
+]);
